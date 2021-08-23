c5a25ecb6dd46a7d3098ae571b9b85462af9f119
Emmanuel Frécon <efrecon@gmail.com>
2021-08-24 00:02:10 +0200

    fix shellcheck

--- bin/img_tags.sh
+++ bin/img_tags.sh
@@ -4,7 +4,7 @@
 set -eu
 
 ROOT_DIR=$( cd -P -- "$(dirname -- "$(command -v -- "$0")")" && pwd -P )
-# shellcheck disable=SC1090
+# shellcheck disable=SC1091
 . "${ROOT_DIR}/../image_tags.sh"
 
 # Call function with same name as script
